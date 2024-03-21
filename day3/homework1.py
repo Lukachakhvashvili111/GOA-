3a5be73a57c7af0e959f0b0b26d4451ba32b71fc
Lukachakhvashvili111 <164405802+Lukachakhvashvili111@users.noreply.github.com>
2024-03-21 23:33:31 +0400

    Add files via upload

---
+++ day3/homework1.py
@@ -0,0 +1,25 @@
+incredebily in your face
+gong 
+ong
+omg
+giga chad
+nika mas
+meowwww
+kqdqw ewanz nd qdsxd
+daaxed axnxne scdijeicxkcndndxnzknzde
+msmssssjsjdc e c esv b engegfdf
+ fvedc efe cdf ff efde fromfr ReferenceError
+ r scxcdccercrecs
+  from django.conf import SettingsReferencef
+  raise
+zzd
+slicecr
+casec
+ varsd xc
+ x xd
+  
+  cases
+  
+  credits
+  dict creditsd
+  set 
