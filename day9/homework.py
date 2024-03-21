9fbc67c597a78d2f510e41afb82c9c4e46dc36af
Lukachakhvashvili111 <164405802+Lukachakhvashvili111@users.noreply.github.com>
2024-03-21 23:42:46 +0400

    Add files via upload

---
+++ day9/homework.py
@@ -0,0 +1,6 @@
+your_age = input('how old are you ? ')
+your_mums_age = input('how old is your mum ? ')
+if your_age > your_mums_age:
+    print('your mum is younger than you,which is phisicly impossible')
+if your_mums_age > your_age:
+    print('your mum is older than you are,now thats more like it :)')
