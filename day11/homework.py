466a93eb77a9b2edd67fb1a3c995b81686db9f9d
Lukachakhvashvili111 <164405802+Lukachakhvashvili111@users.noreply.github.com>
2024-03-21 23:45:11 +0400

    Add files via upload

---
+++ day11/homework.py
@@ -0,0 +1,6 @@
+dads_age = input('how old is your dad ? ')
+mums_age = input('how old is your mum son ? ')
+if dads_age > mums_age:
+    print('your dad is older than your mum')
+if mums_age > dads_age:
+    print('your mum is older than your dad')
