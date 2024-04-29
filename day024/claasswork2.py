55cc06bc451888428bb855e55a6447721a7c1034
Lukachakhvashvili111 <164405802+Lukachakhvashvili111@users.noreply.github.com>
2024-04-29 21:10:06 +0400

    Add files via upload

---
+++ day024/claasswork2.py
@@ -0,0 +1,4 @@
+password = ('goagoa2017')
+enter_password = input('type your password')
+while enter_password == password:
+    print(input('incorrect password,try again'))
---
+++ day024/classwork.py
@@ -0,0 +1,3 @@
+number = int( input("type any number") )
+while number % 5 == 0:
+    print('correct')
